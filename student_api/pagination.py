8991b80af326fd773559d4593751ae1cc00c8554
nihal tekin <nihalpolattekin@gmail.com>
2023-07-31 12:54:39 +0200

    first commit

---
+++ student_api/pagination.py
@@ -0,0 +1,18 @@
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination, CursorPagination
+
+
+class MyNumberPagination(PageNumberPagination):
+    page_size=5 #her sayfa icin max obje sayisi
+    page_query_param="sayfa"  #querydeki key page yerine
+    page_size_query_param="adet"  # dönen veriyi sinirla
+    max_page_size=3
+
+class MyLimitPagination(LimitOffsetPagination):
+    default_limit=8
+    limit_query_param="kac_tane"
+    offset_query_param="kacinci"
+
+class MyCursorPagination(CursorPagination):
+
+    page_size=10
+    ordering="first_name"
